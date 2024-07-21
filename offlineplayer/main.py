447d117f940f5a084ca9644b8cf27c1acfea5b81
GladeOwl <owloftheglade@gmail.com>
2024-07-21 09:54:39 +0530

    Fix Exception

--- offlineplayer/main.py
+++ offlineplayer/main.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
         if songs != None:
             downloader: Downloader = Downloader(songs=songs)
             downloader.start_downloading()
-    except Exception:
-        print(Exception)
+    except Exception as exc:
+        print(exc)
